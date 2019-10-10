d0f531f7b49c13d50b695293d948b682a0ab813b
Jhonatan Catabriga <jhonatancatabriga@gmail.com>
2019-10-10 14:06:02 -0300

    Create Task and TaskList with yours stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/react';
 import '../src/index.css';
+import requireContext from 'require-context.macro';
 
 
-const req = require.context('../src', true, /\.stories.js$/);
+const req = requireContext('../src/components', true, /\.stories.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
